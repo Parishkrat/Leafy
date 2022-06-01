bcf4ae9927ff8ba59e2407ed2af694abcd4a16fa
Parishkrat <97585972+Parishkrat@users.noreply.github.com>
2022-06-01 21:47:07 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,89 @@
+    // set the dimensions and margins of the graph
+    var margin = {top: 30, right: 30, bottom: 30, left: 30},
+      width = 700 - margin.left - margin.right,
+      height = 400 - margin.top - margin.bottom;
+    
+    // append the svg object to the body of the page
+    var svg = d3.select("#heatmap")
+    .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+    
+    // Labels of row and columns
+    var myGroups = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+    var myVars = ["v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8", "v9", "v10"]
+    
+    // Build X scales and axis:
+    var x = d3.scaleBand()
+      .range([ 0, width ])
+      .domain(myGroups)
+      .padding(0.01);
+    svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x))
+    
+    // Build X scales and axis:
+    var y = d3.scaleBand()
+      .range([ height, 0 ])
+      .domain(myVars)
+      .padding(0.01);
+    svg.append("g")
+      .call(d3.axisLeft(y));
+    
+    // Build color scale
+    var myColor = d3.scaleLinear()
+      .range(["white", "#0c27f5"])
+      .domain([1,100])
+    
+    //Read the data
+     d3.csv("https://gist.githubusercontent.com/Parishkrat/24c7d75ffb231d7e07bc17b91b10f66e/raw/895517b937fa7d1321b117d6f453ef284c6e6d6b/gistfile1.txt",function(data) {
+    
+      // create a tooltip
+      var tooltip = d3.select("#heatmap")
+        .append("div")
+        .style("opacity", 0)
+        .attr("class", "tooltip")
+        .style("background-color", "white")
+        .style("border", "solid")
+        .style("border-width", "2px")
+        .style("border-radius", "5px")
+        .style("padding", "5px")
+    
+      // Three function that change the tooltip when user hover / move / leave a cell
+      var mouseover = function(d) {
+        tooltip.style("opacity", 1)
+      }
+      var mousemove = function(d) {
+        tooltip
+          .html("The exact value of<br>this cell is: " + d.value)
+          .style("left", (d3.mouse(this)[0]+70) + "px")
+          .style("top", (d3.mouse(this)[1]) + "px")
+      }
+      var mouseleave = function(d) {
+        tooltip.style("opacity", 0)
+      }
+    
+      // add the squares
+      svg.selectAll()
+        .data(data, function(d) {return d.group+':'+d.variable;})
+        .enter()
+        .append("rect")
+          .attr("x", function(d) { return x(d.group) })
+          .attr("y", function(d) { return y(d.variable) })
+          .attr("width", x.bandwidth() )
+          .attr("height", y.bandwidth() )
+          .style("fill", function(d) { return myColor(d.value)} )
+        .on("mouseover", mouseover)
+        .on("mousemove", mousemove)
+        .on("mouseleave", mouseleave)
+    })
+    const url="https://gist.githubusercontent.com/Parishkrat/24c7d75ffb231d7e07bc17b91b10f66e/raw/895517b937fa7d1321b117d6f453ef284c6e6d6b/gistfile1.txt";
+    const fetchText=async()=>{
+        const response= await fetch(url);
+        const text=await response.text();
+        console.log(text);
+    };
+    fetchText();
