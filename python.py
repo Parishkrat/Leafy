02bd80ca4fd9ca2457af33c976c5a8fa16925b63
Jyotiraditya <68176424+jyotiradityast@users.noreply.github.com>
2022-06-02 13:35:43 +0530

    Added python program to update data.txt

---
+++ python.py
@@ -0,0 +1,27 @@
+from string import ascii_lowercase
+
+while True:
+    column=input("Edit value at:\nColumn: ")
+    row=input("Row: ")
+    new_value=input("New Value: ")
+    loc=[column,row]
+    #print(loc,new_value)
+
+    def calculate_line_index(loc):
+
+        index = ord(loc[0]) - 97
+        calculated=(index*10)+int(loc[1])
+        print("Line Number Calculated: "+str(int(calculated+1)))
+        return calculated
+
+    with open('data.txt', 'r', encoding='utf-8') as file:
+        data = file.readlines()
+    
+    #print(data)
+    line_index= calculate_line_index(loc)
+    data[line_index] = column.upper()+",v"+row+","+new_value+"\n"
+    
+    with open('data.txt', 'w', encoding='utf-8') as file:
+        file.writelines(data)
+
+    print("Updated Value at "+column.upper()+","+row+" | New Value: "+ new_value+"\n")
